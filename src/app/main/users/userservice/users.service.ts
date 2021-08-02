edc48c4dd1ff3ef9d1914a9617c68e5d1e870712
Dinesh <dinesh.dhiman@sourcefuse.com>
2021-08-02 21:28:24 +0530

    final

---
+++ src/app/main/users/userservice/users.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import {Iroutes} from '../modal/routes.modal'
+import {HttpClient, HttpErrorResponse} from '@angular/common/http'
+import { Iusers } from '../modal/user.modal';
+import { Observable , throwError} from 'rxjs';
+import {catchError,retry} from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService implements Iroutes{
+  private baseurl = 'http://localhost:3000';
+
+  constructor(private http:HttpClient) { }
+
+  // private Errorhandle(error:HttpErrorResponse){
+
+  // }
+
+
+
+
+  createuser(user:Iusers):Observable<Iusers>{
+    return this.http.post<Iusers>(`${this.baseurl}` + `/users/post`,user)
+  }
+
+  getuser():Observable<any>{
+    const headers = new HttpHeaders({ 'Access-Control-Allow-Origin': '*',
+    'content-type': 'application/json'});
+  
+
+    return this.http.get(`${this.baseurl}` + `/users/get`,{'headers':headers})
+
+  }
+
+  getuserbyid(id:number):Observable<any>{
+
+    const headers = new HttpHeaders({ 'Access-Control-Allow-Origin': '*',
+    'content-type': 'application/json'});
+  
+
+    return this.http.get(`${this.baseurl}` + `/users/` + id,{'headers':headers})
+
+  }
+
+  updateuser(user:any):Observable<Iusers>{
+    return this.http.put<Iusers>(`${this.baseurl}` + `/users/` + user.id , user)
+  }
+
+  deleteuser(user:Iusers):Observable<Iusers>{
+    const headers = new HttpHeaders({ 'Access-Control-Allow-Origin': '*',
+    'content-type': 'application/json'});
+  
+    return this.http.delete<Iusers>(`${this.baseurl}` + `/users/` + user.id,{headers:headers})
+  }
+
+
+}
